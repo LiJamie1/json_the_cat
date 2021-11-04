af13fa58d3e3f7e6cf425086cd8abab46a85f49f
Jamie Li <lijamie19828@gmail.com>
2021-11-04 21:28:12 +0000

    removed console.log for response

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,9 +4,8 @@ const request = require('request');
 const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     if (error) {
-      callback(error, null);
+      callback(error);
     }
-    console.log('statusCode:', response && response.statusCode);
 
     const data = JSON.parse(body);
     if (data[0] === undefined) {
