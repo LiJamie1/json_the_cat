79df7cefd39d9784f81fbc6eb48af1a67713431c
Jamie Li <lijamie19828@gmail.com>
2021-11-04 20:21:01 +0000

    eslinted

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,20 @@
-const request = require('request')
-const args = process.argv.slice(2)
+const request = require('request');
+const args = process.argv.slice(2);
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
   console.log('error:', error);
-  if(error) {
-    return error
+
+  if (error) {
+    return error;
   }
+
   console.log('statusCode:', response && response.statusCode);
-  const data = JSON.parse(body)
+  const data = JSON.parse(body);
 
-  if (data.length === 0){
+  if (data.length === 0) {
     console.log("Breed Not Found");
-    return;
+    return
   }
 
-  console.log(`${data[0].name}: ${data[0].description}`)
+  console.log(`${data[0].name}: ${data[0].description}`);
 });
